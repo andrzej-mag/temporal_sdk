2c61715bc54831a42169e8c2bd7c6a60d19f9cff
Andrzej Magdziarz <am@am.ventures>
2026-01-21 14:26:42 +0100

    feat: add nexus for [temporal_sdk, cluster, stats] telemetry event

--- src/cluster/temporal_sdk_cluster_telemetry_poller.erl
+++ src/cluster/temporal_sdk_cluster_telemetry_poller.erl
@@ -12,6 +12,7 @@
 
 poll(#{cluster := Cluster} = Metadata, Timeout) ->
     {AW, AWCount} = parse_worker_list(Cluster, activity),
+    {NW, NWCount} = parse_worker_list(Cluster, nexus),
     {WW, WWCount} = parse_worker_list(Cluster, workflow),
     {ok, Stats} = temporal_sdk_cluster:stats(Cluster),
     temporal_sdk_telemetry:spawn_execute(
@@ -20,6 +21,8 @@ poll(#{cluster := Cluster} = Metadata, Timeout) ->
         #{
             activity_list => AW,
             activity_count => AWCount,
+            nexus_list => NW,
+            nexus_count => NWCount,
             workflow_list => WW,
             workflow_count => WWCount,
             stats => Stats
