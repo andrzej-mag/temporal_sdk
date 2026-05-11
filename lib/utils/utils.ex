be4c7953776cbbcdb9194be7c323254c6a7819ad
Andrzej Magdziarz <am@am.ventures>
2026-05-11 13:28:45 +0200

    chore: optimize docs AST external translation

--- lib/utils/utils.ex
+++ lib/utils/utils.ex
@@ -29,7 +29,7 @@ defmodule TemporalSdk.Utils do
 
               case rest1 do
                 [] -> tx_mod(fmod1)
-                _ -> "#{fmod1}:#{rest1}"
+                _ -> mod
               end
 
             true ->
