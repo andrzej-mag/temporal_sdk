fdd4057b89b74c1b165c7c225841ff197a72db52
Andrzej Magdziarz <am@am.ventures>
2026-05-18 19:07:01 +0200

    fix: fix TemporalSdk.Activity __using__ macro

--- lib/temporal_sdk/temporal_sdk_activity.ex
+++ lib/temporal_sdk/temporal_sdk_activity.ex
@@ -1,4 +1,12 @@
 defmodule TemporalSdk.Activity do
   import TemporalSdk.Utils.Code
   delegate_all(from: "src/temporal_sdk/temporal_sdk_activity.erl")
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour :temporal_sdk_activity
+
+      import TemporalSdk.Activity, warn: false
+    end
+  end
 end
